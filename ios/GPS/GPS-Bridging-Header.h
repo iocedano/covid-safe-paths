2987088d91527413b01303c71b071b58d0fc992e
John Schoeman <johnschoeman1617@gmail.com>
2020-05-29 06:47:21 -0400

    wip :wq

---
+++ ios/GPS/GPS-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  GPS-Bridging-Header.h
+//  GPS
+//
+//  Created by John Schoeman on 5/28/20.
+//  Copyright © 2020 Path Check Inc. All rights reserved.
+//
+
+#ifndef GPS_Bridging_Header_h
+#define GPS_Bridging_Header_h
+
+#import "MAURLocation.h"
+
+#endif /* GPS_Bridging_Header_h */
