438dc7689e856d4f0e46589f98dcfb2d5c0a42cf
John Schoeman <johnschoeman1617@gmail.com>
2020-05-29 11:18:53 -0400

    wip

--- ios/GPS/Tmp.swift
+++ ios/GPS/Tmp.swift
@@ -1,12 +1,9 @@
 //
-//  GPS-Bridging-Header.h
+//  Tmp.swift
 //  GPS
 //
 //  Created by John Schoeman on 5/29/20.
 //  Copyright © 2020 Path Check Inc. All rights reserved.
 //
 
-
-#import <MAURLocation.h>
-#import "React/RCTBridgeModule.h"
-
+import Foundation
--- ios/GPS/storage/SecureStorage.swift
+++ ios/GPS/storage/SecureStorage.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import RealmSwift
-import MAURLocation
+
 
 class SecureStorage: NSObject {
   
